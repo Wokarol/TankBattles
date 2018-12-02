375382824e0305ff8f8132d05d735cbc3a269eff
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2018-12-02 21:30:16 +0100

    Centre of mass manipulation test;

---
+++ Tanks Battle/Assets/CentreOfMassEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CentreOfMassEditor : MonoBehaviour
+{
+    new Rigidbody2D rigidbody;
+
+
+    private void Awake() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start() {
+        rigidbody.centerOfMass = new Vector2(0, -1);
+    }
+
+    private void OnDrawGizmos() {
+        if(!rigidbody) rigidbody = GetComponent<Rigidbody2D>();
+
+        Gizmos.DrawWireSphere(transform.position + (Vector3)rigidbody.centerOfMass, 0.2f);
+    }
+}
